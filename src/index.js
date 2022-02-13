b18c097938b4abc093ae7cab1799c6e7f7fd8c03
Лапулина-красотулина <lapulina-krasotulina@Air-Save.Dlink>
2022-02-13 12:20:48 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
 
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
-}
+module.exports = function towelSort (x) {
+    let arr=[];
+    if(!x) return arr;
+  for(let i=0; i<=x.length - 1;i++){
+  if (i==0 || i%2 == 0){
+    console.log(i)
+  arr.push(x[i])
+    }else{
+
+  let reversePart = x[i].reverse();
+  arr.push(reversePart)
+  }
+  }
+  return(arr.flat())
+  }
